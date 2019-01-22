ae45e3032498fa13f9946c895955be77d9a99ab2
katrin07 <Katrin.P@cropx.com>
2019-01-22 22:45:02 +0200

    changes katrin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'meetup-lecturers-client';
 
-<<<<<<< HEAD
-  //Hi there! That's Katrin Polit
-  //Hi all, this is Julia
-=======
+
   //Hi there! That's Katrin
-  //Hi all, this is Julia 
+  //Hi all, this is Julia
   //Hi, this is Yarden
->>>>>>> c79df4506aa79f8e6aa5dbd7f64ed082b1cc5f8f
 }
