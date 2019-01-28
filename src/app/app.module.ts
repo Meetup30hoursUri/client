14c5123e17ad0f17a1fcc8554f356c62df3b6b62
juliasht <juliasht@gmail.com>
2019-01-28 22:13:59 +0200

    Lecturer's profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule,
+  MatButtonToggleModule,MatCardModule,MatDatepickerModule,
+  MatDialogModule, MatDividerModule, MatExpansionModule,
+  MatGridListModule,MatIconModule,MatInputModule,MatChipsModule,
+  MatListModule, MatMenuModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { LecturerComponent } from './lecturer/lecturer.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,8 +21,11 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule, 
-    MatCheckboxModule
+    BrowserAnimationsModule, FormsModule,ReactiveFormsModule,
+    MatButtonModule, MatCheckboxModule ,MatButtonToggleModule,MatCardModule,MatDatepickerModule,
+    MatDialogModule, MatDividerModule, MatExpansionModule,
+    MatGridListModule,MatIconModule,MatInputModule,MatChipsModule,
+    MatListModule, MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/lecturer/lecturer.component.ts
+++ src/app/lecturer/lecturer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 
 @Component({
@@ -7,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lecturer.component.css']
 })
 export class LecturerComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  
   constructor() { }
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 
   ngOnInit() {
   }
