e1773da83df1ee87a4a183ce9a7620cb6921695f
yardenYit <yarden.by@gmail.com>
2019-03-09 23:15:57 +0200

    meetup form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LecturerComponent } from './lecturer/lecturer.component';
+import { MeetupFormComponent } from './meetup-form/meetup-form.component';
 
 const routes: Routes = [
-  { path: 'lecturer', component: LecturerComponent }
+  { path: 'lecturer', component: LecturerComponent },
+  { path: 'meetup-form', component: MeetupFormComponent} 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,39 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import {MatButtonModule, MatCheckboxModule,
-  MatButtonToggleModule,MatCardModule,MatDatepickerModule,
-  MatDialogModule, MatDividerModule, MatExpansionModule,
-  MatGridListModule,MatIconModule,MatInputModule,MatChipsModule,
-  MatListModule, MatMenuModule} from '@angular/material';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule,
+        MatCheckboxModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatExpansionModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatChipsModule,
+        MatListModule,
+        MatMenuModule,
+        MatNativeDateModule,
+        MatSelectModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { LecturerComponent } from './lecturer/lecturer.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './login/login.component';
+import { MeetupFormComponent } from './meetup-form/meetup-form.component';
+import { HttpClientModule }    from '@angular/common/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+import { MeetupService } from './services/meetup/meetup.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LecturerComponent,
-    LoginComponent
+    MeetupFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,9 +43,27 @@ import { LoginComponent } from './login/login.component';
     MatButtonModule, MatCheckboxModule ,MatButtonToggleModule,MatCardModule,MatDatepickerModule,
     MatDialogModule, MatDividerModule, MatExpansionModule,
     MatGridListModule,MatIconModule,MatInputModule,MatChipsModule,
-    MatListModule, MatMenuModule
+    MatListModule, MatMenuModule,MatTabsModule,MatNativeDateModule,
+    MatSelectModule,
+    HttpClientModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firbaseConfig)
+
+    
   ],
-  providers: [],
+  exports: [  
+    MatButtonModule,  
+    MatMenuModule,    
+    MatIconModule,  
+    MatCardModule,  
+    BrowserAnimationsModule,  
+    MatInputModule,  
+    MatDatepickerModule,
+    MatTabsModule,
+    MatNativeDateModule,
+    MatSelectModule
+  ],  
+  providers: [MeetupService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
