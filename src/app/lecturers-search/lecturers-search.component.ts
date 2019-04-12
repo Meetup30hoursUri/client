91b3d9c309a2d4d8fc9bc77065698cfb2a89e70a
KerenC <46246176+KerenC91@users.noreply.github.com>
2019-04-12 11:00:46 +0300

    lecturers table search - phase 2

--- src/app/lecturers-search/lecturers-search.component.ts
+++ src/app/lecturers-search/lecturers-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LecturersService } from 'src/app/services/lecturers-s/lecturers.service';
-import { Subject } from 'rxjs/Subject'
+import { Observable, BehaviorSubject } from 'rxjs'
 
 @Component({
   selector: 'app-lecturers-search',
@@ -8,25 +8,18 @@ import { Subject } from 'rxjs/Subject'
   styleUrls: ['./lecturers-search.component.css']
 })
 export class LecturersSearchComponent implements OnInit {
+  lecturers$: Observable<any[]>;
+  startAt: BehaviorSubject<string | null> = new BehaviorSubject('');
 
-  lecturers;
-  startAt = new Subject()
-  endAt = new Subject()
 
   constructor(private lecturersSvc: LecturersService) { }
 
   ngOnInit() {
-    console.log("ngOnInit of LecturersSearchComponent");
-    this.lecturersSvc.getLecturers(this.startAt, this.endAt)
-    .valueChanges().subscribe(lecturers => this.lecturers = lecturers);
+    this.lecturers$ = this.lecturersSvc.getLecturers(this.startAt);
   }
 
-  search($event) {
-    console.log("search($event)");
-    let q = $event.target.value
-    this.startAt.next(q)
-    this.endAt.next(q+"\uf8ff")
+  search(searchText) {
+    this.startAt.next(searchText);
   }
-
   
 }
--- src/app/services/lecturers-s/lecturers.service.ts
+++ src/app/services/lecturers-s/lecturers.service.ts
@@ -1,7 +1,7 @@
-  import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Observable } from 'rxjs';import {
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { BehaviorSubject, Observable } from 'rxjs';
+import {
   map,
   switchMap,
   debounceTime,
@@ -15,60 +15,29 @@ export class LecturersService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  getLecturers(start, end): AngularFireList<any> {
-
-    // this.db.list('lecturers').valueChanges().subscribe(console.log);
-    // this.db.list('lecturers').query.orderByChild('name').limitToFirst(10).startAt(start).endAt(end);
-    // this.db.list('/lecturers', query.orderByChild('name').limitToFirst(10).startAt(start).endAt(end));
-    console.log("getLecturers of LecturersService:");
-    console.log("start: " + start.name);
-    console.log("end: " + end.name)
-
-    return this.db.list('/lecturers',
-          ref =>
+  getLecturers(start: BehaviorSubject<string>): Observable<any[]> {
+    return start.pipe(
+      switchMap(startText => {
+        const endText = startText + '\uf8ff';
+        return this.db
+          .list('/lecturers', ref =>
             ref
               .orderByChild('name')
               .limitToFirst(10)
-              .equalTo('Gal')
-              // .startAt('Gal').endAt('Gal' + 1)
-              // .endAt('z'.toLowerCase())
-              // .startAt(JSON.stringify(start))
-              // .endAt(JSON.stringify(end))
-          ); 
-    // {
-    //   query: {
-    //     orderByChild: 'name',
-    //     limitToFirst: 10,
-    //     startAt: start,
-    //     endAt: end
-    //   }
-    // }
-    //);
+              .startAt(startText)
+              .endAt(endText)
+          )
+          .snapshotChanges()
+          .pipe(
+            debounceTime(200),
+            distinctUntilChanged(),
+            map(changes => {
+              return changes.map(c => {
+                return { key: c.payload.key, ...c.payload.val() };
+              });
+            })
+          );
+      })
+    );
   }
-
-  // getlecturers(start: BehaviorSubject<string>): Observable<any[]> {
-  //   return start.pipe(
-  //     switchMap(startText => {
-  //       const endText = startText + '\uf8ff';
-  //       return this.db
-  //         .list('/lecturers', ref =>
-  //           ref
-  //             .orderByChild('name')
-  //             .limitToFirst(10)
-  //             .startAt(startText)
-  //             .endAt(endText)
-  //         )
-  //         .snapshotChanges()
-  //         .pipe(
-  //           debounceTime(200),
-  //           distinctUntilChanged(),
-  //           map(changes => {
-  //             return changes.map(c => {
-  //               return { key: c.payload.key, ...c.payload.val() };
-  //             });
-  //           })
-  //         );
-  //     })
-  //   );
-  // }
 }
